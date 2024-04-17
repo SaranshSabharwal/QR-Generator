070137910ad1e2a36d94a80fda1f4f2d7a01e7f1
Saransh Sabharwal <154620242+funkeygamer@users.noreply.github.com>
2024-04-17 13:38:59 +0530

    Delete script.js
     Script was added to the html code to make it more efficient

--- script.js
+++
@@ -1,7 +0,0 @@
-let imgbox = document.getElementById("imgbox");
-let qrImage = document.getElementById("qrImage");
-let qrText = document.getElementById("qrText");
-function generateQR(){
-    qrImage.scr="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.ariaValueMax;
-
-}
